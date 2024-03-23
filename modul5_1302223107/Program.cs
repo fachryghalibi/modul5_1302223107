e5be10167f5e3c2adcca62b04eb9417d0a52aa6f
unknown <fachruddinghalibi@gmail.com>
2024-03-23 08:47:54 +0700

    menambahkan kelas simple database

--- modul5_1302223107/Program.cs
+++ modul5_1302223107/Program.cs
@@ -1,21 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-class Penjumlahan
-{
-    public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
-    {
-        dynamic a = input1;
-        dynamic b = input2;
-        dynamic c = input3;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
-        return a+ b + c; 
-    }
-}
 
-class program
-{
-    static void main(String[] args)
+
+    class Program
     {
-        Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(13, 02, 22));
+        static void Main(String[] args)
+        {
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(13, 02, 22));
+            SimpleDataBase<double> DB = new SimpleDataBase<double>();
+            DB.AddNewData(13);
+            DB.AddNewData(02);
+            DB.AddNewData(22);
+            DB.printAllData();
+        }
     }
-}
+
+    class Penjumlahan
+    {
+        public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+        {
+            dynamic a = input1;
+            dynamic b = input2;
+            dynamic c = input3;
+
+            return (a + b + c);
+        }
+    }
+
+
+
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T newData)
+        {
+            this.storedData.Add(newData);
+            this.inputDates.Add(DateTime.Now);
+        }
+
+        public void printAllData()
+        {
+            for (int i = 0; i < this.inputDates.Count; i++)
+            {
+                Console.WriteLine("Data " + i + " berisi " + this.storedData[i] + " pada waktu UTC " + this.inputDates[i]);
+            }
+        }
+
+    }
+
+
--- modul5_1302223107/obj/Debug/net8.0/modul5_1302223107.AssemblyInfo.cs
+++ modul5_1302223107/obj/Debug/net8.0/modul5_1302223107.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_1302223107")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d24ba78b73de7415c8486f3f354f253f21f640")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db116f851a8a38dab384cad441cd9d957f71d586")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_1302223107")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_1302223107")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
